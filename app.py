80345846d70b615d42802371094e1de883841aca
qrcode1 <36200262+ChenQirui1@users.noreply.github.com>
2021-02-18 13:35:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ def hello():
     return "Hello World!"
     
 #https://www.youtube.com/watch?v=0iB5IPoTDts for reference, skip to 24:00
-    
+#https://learning.postman.com/docs/designing-and-developing-your-api/mocking-data/mocking-with-examples/
+
+
 #/create 
 
 #/read
