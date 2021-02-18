5c0cb07abfc43d5a0a833c9c7991df53e7caef73
qrcode1 <36200262+ChenQirui1@users.noreply.github.com>
2021-02-18 13:30:46 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+    
+#https://www.youtube.com/watch?v=0iB5IPoTDts for reference, skip to 24:00
+    
+#/create 
+
+#/read
+
+#/update PUT
+
+#/delete DEL
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
+#/create 
+
+
+
+
+
+#/create 
+
